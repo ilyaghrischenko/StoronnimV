e507c828b346746ac50cf2c27158fa6bb874e5df
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-25 18:35:21 +0200

    add BaseEntity.cs and News.cs

---
+++ StoronnimV/StoronnimV.Domain/DbModels/BaseEntity.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+
+namespace StoronnimV.Domain.DbModels;
+
+/// <summary>
+/// Базовая сущность, хранит общие свойство для всех остальных: Id, CreatedAt, UpdatedAt
+/// </summary>
+public abstract class BaseEntity
+{
+    public BigInteger Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ StoronnimV/StoronnimV.Domain/DbModels/News.cs
@@ -0,0 +1,23 @@
+using StoronnimV.Domain.Enums;
+
+namespace StoronnimV.Domain.DbModels;
+
+/// <summary>
+/// Сущность, для хранения информации о новости
+/// </summary>
+public class News : BaseEntity
+{
+    public string Photo { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public NewsPriority Priority { get; set; }
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    
+    public News(string photo, string title, string description, NewsPriority priority)
+    {
+        Photo = photo;
+        Title = title;
+        Description = description;
+        Priority = priority;
+    }
+}
