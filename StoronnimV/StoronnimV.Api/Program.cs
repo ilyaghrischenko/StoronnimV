5d17a28d31d88663ee0828f814e75d685b0c72cb
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-25 23:49:03 +0200

    add generic interface and repository

--- StoronnimV/StoronnimV.Api/Program.cs
+++ StoronnimV/StoronnimV.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using StoronnimV.Contracts.Repositories;
 using StoronnimV.Data;
+using StoronnimV.Data.Repositories;
 using StoronnimV.Domain.DbModels;
 using StoronnimV.Domain.Enums;
 
@@ -9,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddDbContextFactory<StoronnimVContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("LocalConnection")));
---
+++ StoronnimV/StoronnimV.Contracts/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using StoronnimV.Domain.DbModels;
+
+namespace StoronnimV.Contracts.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(BigInteger id);
+    Task<IEnumerable<T>?> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity, Action updateAction);
+    Task DeleteAsync(T entity);
+}
