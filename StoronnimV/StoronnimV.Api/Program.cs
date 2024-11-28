635186ebe7d684fb63c308b0e80f573702673001
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-28 19:09:55 +0200

    change logging from .txt to .json

--- StoronnimV/StoronnimV.Api/Program.cs
+++ StoronnimV/StoronnimV.Api/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("../logs/log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("../logs/log.json", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Host.UseSerilog();
