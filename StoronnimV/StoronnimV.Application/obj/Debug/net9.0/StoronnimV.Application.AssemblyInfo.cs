5de1ba7360082b0a42abc2de74810c780b597fdd
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-29 18:38:32 +0200

    test

--- StoronnimV/StoronnimV.Application/obj/Debug/net9.0/StoronnimV.Application.AssemblyInfo.cs
+++ StoronnimV/StoronnimV.Application/obj/Debug/net9.0/StoronnimV.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoronnimV.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3dbd3551a03ac8d550b1e7111c4aff2206a28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a1f0603e6ed4a01f4c57ddc721b58789b1dc3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoronnimV.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoronnimV.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
